3da54d3e291cd7f5705f03ae4b1dbd09db2f939d
janfic <janfic.dev@gmail.com>
2018-10-01 20:37:05 -0400

    made libgdx project

---
+++ source/android/src/com/janfic/projects/devtober2018/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.janfic.projects.devtober2018;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.janfic.projects.devtober2018.DevtoberGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new DevtoberGame(), config);
+	}
+}
---
+++ source/desktop/src/com/janfic/projects/devtober2018/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.janfic.projects.devtober2018.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.janfic.projects.devtober2018.DevtoberGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DevtoberGame(), config);
+	}
+}
